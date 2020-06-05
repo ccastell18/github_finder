3b8b4f1231099ccb5e193ab9508ec57a7c518717
Christopher Castellano <c.castellano18@gmail.com>
2020-06-05 14:30:11 -0500

    user item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <UserItem />
       </div>
     );
   }
