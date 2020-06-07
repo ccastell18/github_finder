90bf06cb005f826b9fb4c4a5d18010f8b38b6c25
Christopher Castellano <c.castellano18@gmail.com>
2020-06-07 14:45:15 -0500

    seach component working

--- src/App.js
+++ src/App.js
@@ -10,15 +10,9 @@ class App extends React.Component {
     users: [],
     loading: false,
   };
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_sectet=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-  //   this.setState({ users: res.data, loading: false });
-  // }
-  //search github users
+
   searchUsers = async (text) => {
+    this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_sectet=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
