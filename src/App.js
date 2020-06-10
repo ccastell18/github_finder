bd2a171199c5a246ee9170bec1bf99586199ea2a
Christopher Castellano <c.castellano18@gmail.com>
2020-06-10 01:19:51 -0500

    user component set up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -13,6 +14,7 @@ class App extends React.Component {
     users: [],
     loading: false,
     alert: null,
+    user: {},
   };
 
   searchUsers = async (text) => {
@@ -33,8 +35,17 @@ class App extends React.Component {
     setTimeout(() => this.setState({ alert: null }), 5000);
   };
 
+  //get user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_sectet=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ user: res.data, loading: false });
+  };
+
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, user } = this.state;
 
     return (
       <Router>
@@ -59,6 +70,18 @@ class App extends React.Component {
                 )}
               />
               <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
